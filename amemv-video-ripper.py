fc012b0b3dad99290fd15c4dceef43bb5401a6c9
liangd <liangding@sensetime.com>
2018-12-03 20:30:15 +0800

    check filesize to avoid incomplete download; add sleep 1s after every download to avoid anticrawling

--- amemv-video-ripper.py
+++ amemv-video-ripper.py
@@ -4,6 +4,7 @@ import os
 import sys, getopt
 
 import urllib.parse
+import urllib.request
 import copy
 import hashlib
 import codecs
@@ -12,6 +13,7 @@ import re
 from six.moves import queue as Queue
 from threading import Thread
 import json
+import time
 
 # Setting timeout
 TIMEOUT = 10
@@ -31,6 +33,23 @@ HEADERS = {
     'user-agent': "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
 }
 
+def getRemoteFileSize(url, proxy=None):
+    '''
+    通过content-length头获取远程文件大小
+    '''
+    try:
+        request = urllib.request.Request(url)
+        request.get_method = lambda: 'HEAD'
+        response = urllib.request.urlopen(request)
+        response.read()
+    except urllib.error.HTTPError as e:
+        # 远程文件不存在
+        print(e.code)
+        print(e.read().decode("utf8"))
+        return 0
+    else:
+        fileSize = dict(response.headers).get('Content-Length', 0)
+        return int(fileSize)
 
 def download(medium_type, uri, medium_url, target_folder):
     headers = copy.copy(HEADERS)
@@ -45,29 +64,34 @@ def download(medium_type, uri, medium_url, target_folder):
         return
 
     file_path = os.path.join(target_folder, file_name)
-    if not os.path.isfile(file_path):
-        print("Downloading %s from %s.\n" % (file_name, medium_url))
-        retry_times = 0
-        while retry_times < RETRY:
-            try:
-                resp = requests.get(medium_url, headers=headers, stream=True, timeout=TIMEOUT)
-                if resp.status_code == 403:
-                    retry_times = RETRY
-                    print("Access Denied when retrieve %s.\n" % medium_url)
-                    raise Exception("Access Denied")
-                with open(file_path, 'wb') as fh:
-                    for chunk in resp.iter_content(chunk_size=1024):
-                        fh.write(chunk)
-                break
-            except:
-                pass
-            retry_times += 1
-        else:
-            try:
-                os.remove(file_path)
-            except OSError:
-                pass
-            print("Failed to retrieve %s from %s.\n" % medium_url)
+    if os.path.isfile(file_path):
+        remoteSize = getRemoteFileSize(medium_url)
+        localSize = os.path.getsize(file_path)
+        if remoteSize == localSize:
+            return
+    print("Downloading %s from %s.\n" % (file_name, medium_url))
+    retry_times = 0
+    while retry_times < RETRY:
+        try:
+            resp = requests.get(medium_url, headers=headers, stream=True, timeout=TIMEOUT)
+            if resp.status_code == 403:
+                retry_times = RETRY
+                print("Access Denied when retrieve %s.\n" % medium_url)
+                raise Exception("Access Denied")
+            with open(file_path, 'wb') as fh:
+                for chunk in resp.iter_content(chunk_size=1024):
+                    fh.write(chunk)
+            break
+        except:
+            pass
+        retry_times += 1
+    else:
+        try:
+            os.remove(file_path)
+        except OSError:
+            pass
+        print("Failed to retrieve %s from %s.\n" % medium_url)
+    time.sleep(1)
 
 
 def get_real_address(url):
@@ -329,7 +353,10 @@ class CrawlerScheduler(object):
                 challenge_video_params['cursor'] = str(cursor)
                 challenge_video_params['_signature'] = self.generateSignature(str(challenge_id) + '9' + str(cursor))
             res = requests.get(challenge_video_url, headers=HEADERS,params=challenge_video_params)
-            contentJson = json.loads(res.content.decode('utf-8'))
+            try:
+                contentJson = json.loads(res.content.decode('utf-8'))
+            except:
+                print(res.content)
             aweme_list = contentJson.get('aweme_list', [])
             if not aweme_list:
                 break
@@ -337,6 +364,7 @@ class CrawlerScheduler(object):
                 aweme['hostname'] = hostname
                 video_count += 1
                 self._join_download_queue(aweme, target_folder)
+                print("number: ", video_count)
             if contentJson.get('has_more'):
                 cursor = contentJson.get('cursor')
             else:
